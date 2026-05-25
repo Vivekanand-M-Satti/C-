e7a8760027ffa20ae44937a6d0f85051e20028dd
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-05-25 10:47:15 +0000

    update

---
+++ templatelib/list.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main() 
+{
+    list <int> LI;
+    list <int>::iterator it; //inserts elements at end of list
+    LI.push_back(4);
+    LI.push_back(5); //inserts elements at beginning of list
+    LI.push_front(3);
+    LI.push_front(5); //returns reference to first element of list
+    it = LI.begin(); //inserts 1 before first element of list
+    LI.insert(it,1);
+    cout<<"All elements of List LI are: ";
+    for(it = LI.begin();it!=LI.end();it++) 
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    LI.reverse();
+    cout<<"All elements of List LI are after reversing: ";
+    for(it = LI.begin();it!=LI.end();it++) 
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl; //removes all occurences of 5 from list
+    LI.remove(5);
+    cout<<"Elements after removing all occurence of 5 from List: ";
+    for(it = LI.begin();it!=LI.end();it++) 
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl; //removes last element from list
+    LI.pop_back(); //removes first element from list
+    LI.pop_front();
+    return 0;
+}
